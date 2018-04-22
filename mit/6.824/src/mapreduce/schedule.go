0b8d2e4ba40099751ad9a4700d20fcdda261bfdf
joyme <jiangpengfei12@gmail.com>
2018-04-22 12:03:48 +0800

    map/reduce part 3完成

--- mit/6.824/src/mapreduce/schedule.go
+++ mit/6.824/src/mapreduce/schedule.go
@@ -1,6 +1,10 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"sync"
+)
 
 //
 // schedule() starts and waits for all tasks in the given phase (mapPhase
@@ -13,22 +17,67 @@ import "fmt"
 //
 func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, registerChan chan string) {
 	var ntasks int
-	var n_other int // number of inputs (for reduce) or outputs (for map)
+	var nOther int // number of inputs (for reduce) or outputs (for map)
 	switch phase {
 	case mapPhase:
 		ntasks = len(mapFiles)
-		n_other = nReduce
+		nOther = nReduce
 	case reducePhase:
 		ntasks = nReduce
-		n_other = len(mapFiles)
+		nOther = len(mapFiles)
 	}
 
-	fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, n_other)
+	fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, nOther)
 
 	// All ntasks tasks have to be scheduled on workers. Once all tasks
 	// have completed successfully, schedule() should return.
 	//
 	// Your code here (Part III, Part IV).
 	//
+
+	var wg sync.WaitGroup
+
+	if phase == mapPhase {
+		log.Println("map任务：", ntasks)
+		// map过程
+		leftTask := ntasks
+		for index, file := range mapFiles {
+			srv := <-registerChan
+			log.Println("map任务编号：", index)
+			taskArgs := DoTaskArgs{JobName: jobName, File: file, Phase: phase, TaskNumber: index, NumOtherPhase: nOther}
+			wg.Add(1)
+			leftTask--
+			// 使用call() 来调用worker协程工作
+			go func(registerChan chan string, rpcname string, args interface{}, leftTask int) {
+				call(srv, rpcname, args, nil)
+				if leftTask > 1 {
+					registerChan <- srv
+				}
+				wg.Done()
+				log.Println("map任务编号：完成")
+			}(registerChan, "Worker.DoTask", taskArgs, leftTask)
+		}
+		log.Println("进入wait")
+		wg.Wait()
+	} else if phase == reducePhase {
+		log.Println("reduce任务：", ntasks)
+		// reduce过程
+		leftTask := ntasks
+		for i := 0; i < nReduce; i++ {
+			srv := <-registerChan
+			log.Println("reduce任务编号：", i)
+			taskArgs := DoTaskArgs{JobName: jobName, File: "", Phase: phase, TaskNumber: i, NumOtherPhase: nOther}
+			wg.Add(1)
+			go func(registerChan chan string, rpcname string, args interface{}) {
+				call(srv, rpcname, args, nil)
+				if leftTask > 1 {
+					registerChan <- srv
+				}
+				wg.Done()
+				log.Println("reduce任务编号完成")
+			}(registerChan, "Worker.DoTask", taskArgs)
+		}
+		// wg.Wait()
+	}
 	fmt.Printf("Schedule: %v done\n", phase)
 }
